a728fd2d5e339a227705e9d2fc403260372e1ff7
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-12-28 00:35:38 -0500

    router outline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
 import './App.css';
 
+import Navbar from './Components/Navbar';
+import HomeContainer from './Containers/HomeContainer';
+import AboutContainer from './Containers/AboutContainer';
+import MenusContainer from './Containers/MenusContainer';
+
+
 function App() {
   return (
     <div className="App">
-
+<Navbar />
+                <Switch>
+                    <Route exact path='/' render={(routerProps) => <HomeContainer {...routerProps} />} />
+                    <Route path='/contact' render={() => <ContactContainer />} />
+                    <Route path='/menu' render={() => <MenusContainer />} />
+                    <Route path='/about' render={() => <AboutContainer />} />
+                </Switch>
+                <footer>copyright © {new Date().toISOString().substring(0, 4)} strikethrough. all rights reserved.</footer>
 
     </div>
   );
