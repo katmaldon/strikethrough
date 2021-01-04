e29296211cd7f60cfa69fc1c18e5caf028710c7a
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2021-01-03 22:02:53 -0500

    router fix

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './serviceWorker';
+
 // import Users from './users'
 // import Contact from './contact'
-// ReactDOM.render(<App />, document.getElementById('root'))
 
-ReactDOM.render((
+
+ReactDOM.render(
     <BrowserRouter>
-      <Link to="/">Home</Link>
-    </BrowserRouter>
-  ), holder);
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals();
