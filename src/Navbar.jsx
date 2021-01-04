438404d6b3559ccf640d56d840cc08c490881939
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2021-01-03 21:35:27 -0500

    navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,13 +5,10 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            <Link to="/"><img className="logonav" src={logonav3} /></Link>
-            <Link to="projects">projects</Link>
-            <Link to="writings">writings</Link>
-            <Link to="/about">about</Link>
-            <Link to="doinggood">doing good</Link>
-            <Link to="/contact">connect</Link>
-            {/* <ThemeSwitch /> */}
+            {/* <Link to="/"><img className="logonav" src={logonav3} /></Link> */}
+            <Link to="menu">menu</Link>
+            <Link to="/gotem">got 'em</Link>
+            <Link to="/">home</Link>
         </div>
     )
 }
